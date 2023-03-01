4889aa6e616adda68a4e758e5d59669a63327c84
Seiji Chew <schew@G6CH262T6T.local>
2023-03-01 15:36:44 -0800

    fixing pylance error message

--- tester.py
+++ tester.py
@@ -1,3 +1,5 @@
+# type: ignore
+
 import sentry_sdk
 sentry_sdk.init(
     dsn="https://a9ee78acce5c4aa1a36cb471e9ba5d23@o4504765715316736.ingest.sentry.io/4504765725671424",
@@ -5,10 +7,7 @@ sentry_sdk.init(
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0
-
-    # Alternatively, to control sampling dynamically
-    traces_sampler=traces_sampler
+    traces_sample_rate=1.0,
 )
 
 
